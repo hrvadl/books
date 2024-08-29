d91aa491eb411db086cd74b2f0f8ad620f9647e5
Vadym Hrashchenko <vhrashch@cisco.com>
2024-08-29 20:48:24 +0300

    feat: add review pkg

---
+++ internal/domain/review/review.go
@@ -0,0 +1,9 @@
+package review
+
+type Review struct {
+	ID       int
+	BookID   int
+	AuthorID int
+	Rating   int
+	Text     string
+}
---
+++ internal/domain/review/service_test.go
@@ -0,0 +1,15 @@
+package review_test
+
+import "testing"
+
+func TestGetUserReviews(t *testing.T) {
+	type args struct{}
+
+	type fields struct{}
+
+	tc := []struct {
+		name   string
+		args   args
+		fields fields
+	}{}
+}
